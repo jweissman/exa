b55517b304ec616e05d8ba701b9a1396735480fa
Joseph Weissman <joe@bignerdranch.com>
2016-12-20 16:08:17 -0500

    basic recursive data structure

--- lib/exa.rb
+++ lib/exa.rb
@@ -1,1 +1,65 @@
 require 'exa/version'
+
+module Exa
+  class TreeNode
+    attr_reader :name, :value, :children, :parent
+    def initialize(name:, value: '--', parent: nil)
+      @name = name
+      @value = value
+      @parent = parent
+      @children = []
+    end
+
+    def update(val)
+      @value = val
+      self
+    end
+
+    def create_child(name:)
+      child = TreeNode.new(name: name, parent: self)
+      @children << child
+      child
+    end
+  end
+
+  class Visitor
+    def initialize(root)
+      @root = root
+    end
+
+    def seek(path)
+      _root, *path_segments = path.split('/')
+      current = @root
+      path_segments.each do |segment|
+        next_child = current.children.detect do |child|
+          child.name == segment
+        end
+
+        current = if next_child
+          next_child
+        else
+          current.create_child(name: segment)
+        end
+      end
+      current
+    end
+  end
+
+  class << self
+    def remember(path, value)
+      recall(path).update(value)
+    end
+
+    def recall(path)
+      visitor.seek(path)
+    end
+
+    def visitor
+      @visitor ||= Visitor.new(root)
+    end
+
+    def root
+      @root ||= TreeNode.new(name: '', value: '(system root)')
+    end
+  end
+end
--- spec/exa_spec.rb
+++ spec/exa_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
+require 'pry'
 require 'exa'
 
 describe Exa do
-  it "should have a VERSION constant" do
-    expect(subject.const_get('VERSION')).to_not be_empty
+  it 'should store a value' do
+    Exa.remember('/x', 5)
+    Exa.remember('/y', 7)
+    expect(Exa.recall('/x').value).to eq(5)
+    expect(Exa.recall('/y').value).to eq(7)
+  end
+
+  it 'should store a structured value' do
+    Exa.remember('/hello/world', 5)
+    expect(Exa.recall('/hello/world').value).to eq(5)
+  end
+
+  it 'should navigate' do
+    Exa.remember("/a/b/c", 'd')
+    expect(Exa.recall("/a/b/c").value).to eq('d')
+    expect(Exa.recall("/a").children).to eq([Exa.recall("/a/b")])
+    expect(Exa.recall("/a").parent).to eq(Exa.recall("/"))
   end
 end
